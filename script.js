25e398c548b9bccfa65a9266c37b7ffa4107cef0
Luschi25 <147149141+Luschi25@users.noreply.github.com>
2026-05-30 14:40:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ window.onload = function() {
 
         speechSynthesis.speak(speech);
     }
-};
+}; 
+
+function speakText() {
+
+    const textElement = document.getElementById("speechText");
+
+    if(textElement){
+
+        const text = textElement.innerText;
+
+        const speech = new SpeechSynthesisUtterance(text);
+
+        speech.lang = "de-DE";
+        speech.rate = 1.0;
+        speech.pitch = 1.0;
+
+        speechSynthesis.speak(speech);
+    }
+}
